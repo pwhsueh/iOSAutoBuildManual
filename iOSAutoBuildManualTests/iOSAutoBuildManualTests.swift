1181b217814e505b61204e77d81e2dc0dc0f4580
Bowen <pwhsueh@gmail.com>
2016-12-13 16:06:23 +0800

    add test case

--- iOSAutoBuildManualTests/iOSAutoBuildManualTests.swift
+++ iOSAutoBuildManualTests/iOSAutoBuildManualTests.swift
@@ -11,9 +11,12 @@ import XCTest
 
 class iOSAutoBuildManualTests: XCTestCase {
     
+    var numberOperationUtility : NumberOperationUtility?
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        numberOperationUtility = NumberOperationUtility()
     }
     
     override func tearDown() {
@@ -21,9 +24,20 @@ class iOSAutoBuildManualTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testAddition() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let additionResult = numberOperationUtility?.addTwoNumbers(firstNumber: 100, secondNumber: 200)
+        XCTAssertTrue(additionResult==300,"Addition Test Failed");
+    }
+    
+    func testMultiplication() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let multiplicationResult = numberOperationUtility?.multiplyTwoNumbers(firstNumber: 100, secondNumber: 200)
+        XCTAssertTrue(multiplicationResult==20000,"Multiplication Test Failed");
     }
     
     func testPerformanceExample() {
