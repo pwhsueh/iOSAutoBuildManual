4c9b04a01092a741dddf702f06331ac7c27f9530
Bowen <pwhsueh@gmail.com>
2016-12-13 16:03:35 +0800

    add test case

---
+++ iOSAutoBuildManual/NumberOperationUtility.swift
@@ -0,0 +1,23 @@
+//
+//  NumberOperationUtility.swift
+//  iOSAutoBuildDemo
+//
+//  Created by Powen Hsueh on 13/12/2016.
+//  Copyright © 2016 ApPedia. All rights reserved.
+//
+
+import Foundation
+
+
+class NumberOperationUtility {
+    
+    
+    func addTwoNumbers(firstNumber:Int , secondNumber:Int) -> Int {
+        return firstNumber + secondNumber
+    }
+    
+    func multiplyTwoNumbers(firstNumber:Int , secondNumber:Int) -> Int {
+        return firstNumber * secondNumber
+    }
+    
+}
